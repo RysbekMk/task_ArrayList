e6b9fb6fffa72ab7bb76a52c9a6fef2fb76123cd
kasymbaevtimur <temuchi50@gmail.com>
2023-08-13 12:39:25 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+    /**
+     * Жогоруда жазылган статичный полялар биз консолго чыгарып жаткан кодубузга
+     * цвет кошуп берет, астыдагы кодту коментарийден чыгарып иштетип кор
+     * Ps: бул цветтер жон гана сиздерге кызык болуусу учун эле корсотулду тапшырмага тиешеси жок,
+     * бирок ушул цветтерди колдонуп консолго чыгарсанар болот
+     */
+    public static void main(String[] args) {
+
+
+//        System.out.println(ANSI_BLUE+"Кандайсын Студент 🙂"+ANSI_RESET);
+//        System.out.println(ANSI_PURPLE+"Сен бул жердеги задачаны чыгара аласын мен ишенем 😎"+ANSI_RESET);
+//        System.out.println(ANSI_CYAN+"Good luck! 💪");
+//        System.out.println("Have a nice weekend 😀"+ANSI_RESET);
+
+    }
+}
---
+++ src/Student.java
@@ -0,0 +1,16 @@
+/**
+ * Бул классты толуктоо керек коментарийде болгон поляларды чыгарып,
+ * жетишпей жаткан enum'дарды тузуу керек.
+ * Дагы эмнелер жетишпей жатат бул жерде, а может геттер сеттер методтор, а дагы ?
+ *
+ */
+public class Student {
+    private int id;
+    private String name;
+    private String surname;
+    private int age;
+//    private Gender gender; //enum
+
+//    private StudyFormat studyFormat; // enum
+
+}
